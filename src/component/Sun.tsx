963d219f4ac54d210172be2676cf0d2da1b3c31b
ghannoum74 <abdelrahmangh74@gmail.com>
2024-07-31 22:04:47 +0300

    add glow for the sun

--- src/component/Sun.tsx
+++ src/component/Sun.tsx
@@ -1,24 +1,44 @@
+import React, { useRef } from "react";
 import { useFrame, useLoader } from "@react-three/fiber";
-import { useRef } from "react";
 import { TextureLoader } from "three";
 import * as THREE from "three";
-interface sunProps {
+
+// Sun Component
+interface SunProps {
   textureUrl: string;
 }
-const Sun: React.FC<sunProps> = ({ textureUrl }) => {
+
+const Sun: React.FC<SunProps> = ({ textureUrl }) => {
   const sunTexture = useLoader(TextureLoader, textureUrl);
   const sunRef = useRef<THREE.Mesh | null>(null);
+
   useFrame(() => {
+    // Rotate around itself
     if (sunRef.current) {
       sunRef.current.rotation.y += 0.005;
     }
   });
 
   return (
-    <mesh ref={sunRef}>
-      <sphereGeometry args={[1, 64, 64]} />
-      <meshStandardMaterial map={sunTexture} side={THREE.DoubleSide} />
-    </mesh>
+    <group>
+      {/* Inner Sphere */}
+      <mesh>
+        <sphereGeometry args={[1, 64, 64]} />
+        <meshStandardMaterial map={sunTexture} side={THREE.DoubleSide} />
+      </mesh>
+
+      {/* Outer Glowing Sphere */}
+      <mesh ref={sunRef} scale={[1.1, 1.1, 1.1]}>
+        <sphereGeometry args={[1, 64, 64]} />
+        <meshStandardMaterial
+          map={sunTexture}
+          emissive={new THREE.Color("#FDB813")}
+          emissiveIntensity={2}
+          emissiveMap={sunTexture}
+          side={THREE.DoubleSide}
+        />
+      </mesh>
+    </group>
   );
 };
 export default Sun;
