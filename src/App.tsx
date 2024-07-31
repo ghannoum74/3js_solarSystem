86f5c57737b0e2767ea7c05d43a811b82b03e495
ghannoum74 <abdelrahmangh74@gmail.com>
2024-07-31 22:05:45 +0300

    add the effect composer for the glow

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,29 @@ import { Canvas } from "@react-three/fiber";
 import "./css/App.css";
 import { OrbitControls } from "@react-three/drei";
 import Earth from "./component/Earth.js";
+import Sun from "./component/Sun.js";
+import { Bloom, EffectComposer } from "@react-three/postprocessing";
 
 const App = () => {
   return (
     <div>
       <Canvas>
         <ambientLight />
-        <Earth textureUrl="earth_color_10K.png" />
         <OrbitControls />
+        <group>
+          <Earth textureUrl="earth_color_10K.png" />
+          <Sun textureUrl="euvi_aia304_2012_carrington.png" />
+        </group>
+        <EffectComposer>
+          <Bloom
+            intensity={1.5}
+            width={256}
+            height={256}
+            kernelSize={4}
+            luminanceThreshold={0.1}
+            luminanceSmoothing={0.1}
+          />
+        </EffectComposer>
       </Canvas>
     </div>
   );
